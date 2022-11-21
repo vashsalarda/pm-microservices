f66631008f0d429bb1c556d4a9634e7bcf0a7bbb
Vash Salarda <vashful.v2@gmail.com>
2022-11-21 16:57:21 +0800

    removed db.EnsureDeleted

--- Customer/CustomerAccount.API/Program.cs
+++ Customer/CustomerAccount.API/Program.cs
@@ -28,7 +28,6 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
 	var db = scope.ServiceProvider.GetRequiredService<CustomerAccountContext>();
-	db.Database.EnsureDeleted();
     db.Database.Migrate();
 }
 
